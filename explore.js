44909051a23095005e81de1c310a3dab61498d47
mc0749050 <131736467+mc0749050@users.noreply.github.com>
2024-06-23 23:02:01 +0530

    Update explore.js

--- explore.js
+++ explore.js
@@ -10,12 +10,6 @@ function maketripdivon(){
   document.getElementById("maketripdiv").style.display = "block";
 }
 
-document.getElementById('form').addEventListener('submit', function(event) {
-            event.preventDefault();
-            // Your code here to handle the form data
-            document.getElementById("requestsubmit").style.display = "block"
-        });
-
 //email send
 function sendMail(){
       (function(){
@@ -38,59 +32,7 @@ function sendMail(){
       .catch();
       
 
-    a = document.getElementById("name").value;
-    b = document.getElementById("email").value;
-    c = document.getElementById("city").value;
-    d = document.getElementById("ph").value;
-    e = document.getElementById("num").value;
-    
-    if (a == "" || b == "" || c == "" || d == "" || e == "") {
-      console.log("enter data")
-    }
-    else {
-         
-   let parent = document.getElementById("notificationdiv");
-   let newdate = new Date();
-   let hour = newdate.getHours();
-   let minute = newdate.getMinutes();
-   let newformat = hour >= 12 ? 'PM' : 'AM';
-
-    // Find current hour in AM-PM Format
-    hour = hour % 12;
-
-    // To display "0" as "12"
-    hour = hour ? hour : 12;
-    minute = minute < 10 ? '0' + minute : minute;
-
-
-   const newDiv = document.createElement('div');
-   newDiv.classList.add("removenotif");
-    newDiv.innerHTML = ` <div class="notification">
-      <div id="notification-head">
-    <p id="time"><span id="hour">${hour}</Span>:<span id="minute">${minute}</span></p>
-    <i onclick="closenotification()" class="fa-solid fa-xmark"></i>
-    </div>
-    <p id="massage">Your <span id="massagecity">${document.getElementById("city").value}</span> trip request has been submitted, Our team will be contact you soon.</p>
-    </div>`
-    
-      parent.appendChild(newDiv);
-      
-   document.getElementById("nobell").style.display = "none";
-   document.getElementById("nonotif").style.display = "none";
-    
-      document.getElementById("maketripdiv").style.display = "none";
-    }
+   
     }
     
-    function notificationon() {
-      document.getElementById("notificationdiv").style.display = "block";
-    }
-function notificationoff() {
-      document.getElementById("notificationdiv").style.display = "none";
-    }
-
-function closenotification() {
-  document.querySelector(".removenotif").style.display = "none";
-  document.getElementById("nobell").style.display = "block";
-  document.getElementById("nonotif").style.display = "block";
-}
+    
